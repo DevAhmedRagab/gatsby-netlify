50c81a7d75fb8e98bc4f8d306f7096b53a968509
Ahmed Ragab <ahmedragab6781@gmail.com>
2020-12-07 15:34:13 +0200

    Update/blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `FunBits`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Ahmed Ragab`,
+      summary: `who lives and works in Egypt building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    siteUrl: `https://laughing-poincare-6b28ea.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `Ahmed Ragab`,
     },
   },
   plugins: [
